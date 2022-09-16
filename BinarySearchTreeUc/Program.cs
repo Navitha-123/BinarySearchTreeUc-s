59ab10d98b4d487de982bf62942fe5e4bc8969ab
navitha <navithareddy112@gmail.com>
2022-09-16 21:20:56 +0530

    [navitha] UC3SearchBinaryTree

--- BinarySearchTreeUc/Program.cs
+++ BinarySearchTreeUc/Program.cs
@@ -10,7 +10,7 @@ namespace BinarySearchTreeUc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("choose option 1.create binary tree \t 2.Binarytree creation");
+            Console.WriteLine("choose option 1.create binary tree \t 2.Binarytree creation \t 3. searchbinarytree");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -42,6 +42,28 @@ namespace BinarySearchTreeUc
 
                     tree1.Getsize();
 
+                    break;
+
+                case 3:
+                    UC3SearchBinaryTree<int> tree2 = new UC3SearchBinaryTree<int>(56);
+                    tree2.Insert(30);
+                    tree2.Insert(70);
+                    tree2.Insert(22);
+                    tree2.Insert(40);
+                    tree2.Insert(60);
+                    tree2.Insert(95);
+                    tree2.Insert(11);
+                    tree2.Insert(65);
+                    tree2.Insert(3);
+                    tree2.Insert(16);
+                    tree2.Insert(63);
+                    tree2.Insert(67);
+
+                    tree2.Display();
+
+                    tree2.Getsize();
+
+                    bool result = tree2.IfExists(40, tree2);
                     break;
                 default:
                     Console.WriteLine("enter wrong choice");
---
+++ BinarySearchTreeUc/UC3SearchBinaryTree.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeUc
+{
+    public  class UC3SearchBinaryTree<T> where T:IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public UC3SearchBinaryTree<T> LeftTree { get; set; }
+        public UC3SearchBinaryTree<T> RightTree { get; set; }
+        public UC3SearchBinaryTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        int leftcount = 0;
+        int rightcount = 0;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new UC3SearchBinaryTree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new UC3SearchBinaryTree<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftcount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightcount++;
+                this.RightTree.Display();
+            }
+        }
+
+        public void Getsize()
+        {
+            Console.WriteLine("size" + ":" + (1 + this.leftcount + this.rightcount));
+        }
+
+        public bool IfExists(T element, UC3SearchBinaryTree<T> node)
+        {
+            if (node == null)
+            {
+                Console.WriteLine("Element not found in binary search tree");
+                return false;
+            }
+            if(node.NodeData.Equals(element))
+            {
+                Console.WriteLine("found the element in binary search tree " +node.NodeData);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("current element is {0} in binary search tree",node.NodeData);
+
+            }
+            if(element.CompareTo(node.NodeData) < 0)
+            {
+                IfExists(element, node.LeftTree);
+            }
+            if(element.CompareTo(node.NodeData) > 0)
+            {
+                IfExists(element, node.RightTree);
+            }
+            return true;
+        } 
+    }
+}
